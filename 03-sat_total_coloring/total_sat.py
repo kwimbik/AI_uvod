cdcab67040d7f0c0307985c9712decd2681ded42
kwimbik <kwimbiks@gmail.com>
2022-03-10 11:50:41 +0100

    oprava

--- 03-sat_total_coloring/total_sat.py
+++ 03-sat_total_coloring/total_sat.py
@@ -12,18 +12,25 @@ def total_coloring(graph):
         Find the total chromatic index.
     """
 
+    #finds max degree of graph, CN cannot be smaller
     maxDegree = 0
     for u in graph.nodes():
         maxDegree = max(maxDegree, graph.degree[u])
+
     model = None
     CN = maxDegree
+
+    #Every variable has list of possible colors. Index of first positive literal in that list is nummber of color assinged to the edge/node
     while model == None:
         g = Solver()
+        #counts used variables, assigns next edge/vertex next ones avalible
         globalCounter = 1
         CN  += 1
+        #every node/edge has list of its variables -> colors
         variableDict = {}
         edgesStarterPoint = 0
         for u in graph.nodes():
+            #Edges has -1 signature, in case some nodes are tuples, protecting SAT from conflicts
             variableDict[u] = range(globalCounter, globalCounter +CN)
             globalCounter+= CN
 
@@ -42,17 +49,20 @@ def total_coloring(graph):
                 g.add_clause([-variableDict[(u,v,-1)][l], -variableDict[u][l]])
 
 
+        #This can be done faster by looping only neighbors
         for u,v in graph.edges():
              for y,z in graph.edges():
                #add constraint for edges & change for duplicates
                if ((u == y and v != z) or (u != y and v == z) or (u == z and v != y) or (u != z and v == y)):
                    for l in range(CN):
+                       #neighbor edges must have different colors
                        g.add_clause([-variableDict[(u,v,-1)][l], -variableDict[(y,z,-1)][l]])
         
                 
         #every edge and vertice must have color
         for key in variableDict:
                 g.add_clause(variableDict[key])
+        g.solve()
         model = g.get_model()
 
     #COloring of vertices
