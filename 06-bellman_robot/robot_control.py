b06f7b4953307d13b7725669c6f4adf6dbeafb32
kwimbik <kwimbiks@gmail.com>
2022-04-27 10:07:28 +0200

    bellman

--- 06-bellman_robot/robot_control.py
+++ 06-bellman_robot/robot_control.py
@@ -1,6 +1,7 @@
 import numpy, scipy, networkx # You can use everything from these libraries if you find them usefull.
 import scipy.sparse as sparse # Calling scipy.sparse.csc_matrix does not work on Recodex, so call sparse.csc_matrix(...) instead
 import scipy.sparse.linalg as linalg # Also, call linalg.spsolve
+import numpy as np
 
 """
     TODO: Improve the strategy controlling the robot.
@@ -31,22 +32,35 @@ class RobotControl:
 
     # Place all your precomputation here.
     def precompute_probability_policy(self):
-        return self.precompute_probability_policy_trivial()
-
-    # Returns a trivial control strategy which just heads directly toward the station ignoring all dangers and movement imperfectness
-    def precompute_probability_policy_trivial(self):
         env = self.env
+        safetyMap = env.safety_map
+        destination = env.destination
         survivability = numpy.zeros((env.rows, env.columns)) # No probability is computed
+        survivability[tuple(destination)] = 1 #initial survivability is zero except destination point
         policy = numpy.zeros((env.rows, env.columns), dtype=int)
-        for i in range(env.rows):
-            for j in range(env.columns):
-                if i > env.destination[0]:
-                    policy[i,j] = env.NORTH
-                elif i < env.destination[0]:
-                    policy[i,j] = env.SOUTH
-                elif j < env.destination[1]:
-                    policy[i,j] = env.EAST
-                elif j > env.destination[1]:
-                    policy[i,j] = env.WEST
-        return survivability, policy
+        actions = [env.FORWARD,env.EAST, env.BACKWARD, env.WEST]
+        actionsProbability = [env.forward_probability, env.right_probability, env.backward_probability, env.left_probability]
+        rotation = env.ROTATION
 
+        policyUpdated = True
+        next_state_reward = np.zeros(4)
+        #policy update
+        while policyUpdated:
+            policyUpdated = False #we can stop computation once no change in policy is made
+            for i in range(1,env.rows-1):
+                for j in range(1,env.columns-1):
+                        if (i,j) == tuple(destination): continue
+                        #for every cell, compute reward function u = as P(moveToNextCell)*R(nextCell)*safety(nextCell)
+                        for x, rot in enumerate(rotation):
+                            reward = 0
+                            for action in actions:
+                                nextCell = i + env.DIRECTION[rot[action]][0], j + env.DIRECTION[rot[action]][1]
+                                reward += survivability[nextCell] * safetyMap[nextCell]*actionsProbability[action]
+                            next_state_reward[x] = reward
+                        #calculateing maxium of rewards and choosing policy for that action
+                        #if policy is updated, keep iterating
+                        bestMove = np.argmax(next_state_reward)
+                        if (policy[i,j] != bestMove) : policyUpdated = True
+                        policy[i,j] = bestMove
+                        survivability[i,j] = next_state_reward[bestMove]
+        return survivability, policy
